0e533323e6c0e6f9032aedf678ab14b87661b20e
Ihor Shliakhovyi <blizneci2106@gmail.com>
2024-04-22 13:53:12 +0300

    Homework2

--- src/main/java/Lesson2/Homework2.java
+++ src/main/java/Lesson2/Homework2.java
@@ -2,9 +2,19 @@ package Lesson2;
 
 public class Homework2 {
     public static void main(String[] args) {
-        int [] numberArray = new int[]{1,5,6,8,9,13,11,255,36,70,28,111, 165, 54, 75,33,18, 12, 6, 4};
-
-
-
+        int [] numberArray = new int[]{1,5,6,8,9,13,11,255,36,70,-28,111, 165, 54, 75,33,18, 12, 6, 4};
+        for(int i=0; i < numberArray.length; i++){
+//            All positives even numbers
+            if(numberArray[i]%2==0 && numberArray[i]>=0){
+                System.out.print(numberArray[i] + " ");
+            }
+        }
+        int summ = 0;
+        for (int i=0; i < numberArray.length; i++){
+            if(numberArray[i]>5 && numberArray[i]<45){
+                summ += numberArray[i];
+            }
+        }
+        System.out.println("\nSumm of numbers above 5 and below 45: "+summ);
     }
 }
