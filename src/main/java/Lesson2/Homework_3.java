02a8f0b69ca491e0cf36a55ab939f4687f73acef
Ihor Shliakhovyi <blizneci2106@gmail.com>
2024-04-22 14:10:10 +0300

    Homework3

--- src/main/java/Lesson2/Homework_3.java
+++ src/main/java/Lesson2/Homework_3.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Homework_3 {
     public static void main(String[] args) {
+//        Display array in original and sorted view
         int [] numberArray = new int[]{1,5,6,8,9,13,11,255,36,70,-28,111, 165, 54, 75,33,18, 12, 6, 4};
         System.out.println("The original array is: ");
         for(int i=0; i<numberArray.length; i++){
